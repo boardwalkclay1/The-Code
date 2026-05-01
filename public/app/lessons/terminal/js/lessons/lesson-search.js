781d01daaa0865af6e8ae9be1b154076fe1d3659
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-05-01 18:26:36 -0400

    Create lesson-search.js

---
+++ public/app/lessons/terminal/js/lessons/lesson-search.js
@@ -0,0 +1,6 @@
+export const LessonSearch = {
+    find(keyword) {
+        window.TerminalCore.print(`Searching lessons for: ${keyword}`);
+        window.TerminalCore.print("Search engine coming soon.");
+    }
+};
